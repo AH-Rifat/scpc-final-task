cf0445b649c00a77037c20547ed49cc07cebbf04
ah-rifat <rifat266@gmail.com>
2023-07-27 21:29:30 +0600

    task 4 done

---
+++ task4.js
@@ -0,0 +1,22 @@
+// Create a function that takes a sorted array of numbers and a target value as input. The function should find two numbers in the array that add up to the target value. Return an array containing the indices of the two numbers.
+function findTwoNumbersWithSum(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        const sum = arr[left] + arr[right];
+
+        if (sum === target) {
+            return [left, right];
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return [];
+}
+
+const result = findTwoNumbersWithSum([1, 3, 6, 8, 11, 15], 9);
+console.log("Indices of two numbers with sum:", result);
