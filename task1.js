1a1409e0bd52dd2dcf5e4be2b1a617b1fae260e7
ah-rifat <rifat266@gmail.com>
2023-07-27 20:23:43 +0600

    task 1 done

---
+++ task1.js
@@ -0,0 +1,12 @@
+// Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method.
+
+function reverseString(str) {
+    let reversedStr = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr += str[i];
+    }
+    return reversedStr;
+}
+
+const inputString = "hello world";
+console.log(reverseString(inputString));
